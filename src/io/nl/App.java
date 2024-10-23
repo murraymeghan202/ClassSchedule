82ed22e1889ac50d4f82803a390eb3adbd7a1234
Patron <Patron@CCLDPATRON2.LOCAL>
2024-10-23 11:23:32 -0400

    Created basic scheme for creating a JSON schedule

--- src/io/nl/App.java
+++ src/io/nl/App.java
@@ -1,5 +1,8 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nl.Schedule;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 
 public class App {
   public static void main(String[[] args) throws Exception {
@@ -7,8 +10,23 @@ public class App {
     ObjectMapper map = new ObjectMapper();
 
     //Create the JSON object
-	Schedule schedule = new Schedule("Pre Algebra", "Mr. Kizics", 1500);
+	Scanner cname = new Scanner(System.in);
+	System.out.println("Enter your name: ")
+	String name = cname.nextLine();
+	System.out.println("Enter Class Name: ")
+	String class_name = cname.nextLine();
+	System.out.println("Enter Teacher's Name: ")
+	String teacher = cname.nextLine();
+	System.out.println("Enter class time: ")
+	int time = Integer.parseInt(cname.nextLine());
+	
+	Schedule schedule = new Schedule(class_name, teacher, time;
 	String json_string = map.writeValueAsString(schedule);
+	
+	//Write the Schedule to JSON
+	File schedule = new File(System.getProperty("user.dir") + "/schedules/" + name))l
+	FileWriter schedule_writer = new FileWriter.write(schedule, json_string));
+  }
 	
 	
     
