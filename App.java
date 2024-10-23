1ed7ebc8cfdb93e649080fddf2c355d76d0a8705
murraymeghan202 <murrayalex2024.5@gmail.com>
2024-10-23 10:50:27 -0400

    Update App.java

--- App.java
+++ App.java
@@ -1,3 +1,19 @@
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import 
 
-Gson gson = new Gson();
+public class App {
+  public static void main(String[[] args) throws Exception {
+
+    ObjectMapper map = new ObjectMapper();
+
+    //Create the JSON object
+    JSONObject object = new JSONObject();
+    object.put("teacher", args[0]);
+    object.put("class_name", args[1]);
+    object.put("time", args[2]);
+
+    //Convert the JSON Object to a String
+    String json_string = object.toString();
+    
+    
+    
