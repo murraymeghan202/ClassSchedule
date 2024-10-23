e7259845a24ebea2d7c576492c1857933947b368
murraymeghan202 <murrayalex2024.5@gmail.com>
2024-10-23 10:43:00 -0400

    Create Schedule.java

---
+++ Schedule.java
@@ -0,0 +1,33 @@
+
+static class Schedule {
+	private String class_name;
+	private String teacher;
+	private int time;
+
+	public String getClassName() {
+		return class_name;
+	}
+
+	public void setClassName(String class_name);
+		this.class_name = class_name;
+	}
+
+	public String getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(String class_name);
+		this.teacher = teacher;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+};
+
+	
+	
